aef69cd36dcc0da74e6f33805fc2727bef274822
olaa30-j <olaadel.967@gmail.com>
2025-05-25 06:24:44 +0300

    feat: setup vercel (add vercel.json)

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,6 @@ dotenv.config();
 import app from "./app";
 import { connectDB, PORT } from "./config/db";
 
-//4_start server
 connectDB();
 app.listen(PORT, () => {
   console.log(`listen to port ${PORT}`);
